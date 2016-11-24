6ce4d9960780f6c496979c00830c7f300a55ddd8
Tony Bowden <tony@tmtm.com>
2016-11-24 12:24:33 +0000

    run rubocop -a

--- scraper.rb
+++ scraper.rb
@@ -1,5 +1,6 @@
 #!/bin/env ruby
 # encoding: utf-8
+# frozen_string_literal: true
 
 require 'csv'
 require 'open-uri'
@@ -11,7 +12,7 @@ OpenURI::Cache.cache_path = '.cache'
 
 class String
   def tidy
-    self.gsub(/[[:space:]]+/, ' ').strip
+    gsub(/[[:space:]]+/, ' ').strip
   end
 end
 
@@ -25,27 +26,27 @@ end
 def reprocess_csv(file)
   arabic = {}
 
-  raw = open(file).read.force_encoding("UTF-8")
+  raw = open(file).read.force_encoding('UTF-8')
   csv = CSV.parse(raw.lines.drop(2).join)
-  csv.each_with_index do |row, i|
+  csv.each_with_index do |row, _i|
     next if row[0].to_s.empty?
     row = row.map(&:to_s).map(&:tidy)
 
     party, party_id = row[4].match(/(.*?) \((.*?)\)/).captures
 
     data = {
-      name: "%s %s" % [row[1].to_s.tidy, row[0].to_s.tidy],
-      sort_name: "%s %s" % [row[0].to_s.tidy, row[1].to_s.tidy],
-      given_name: row[1].to_s.tidy,
-      family_name: row[0].to_s.tidy,
-      name__ar: "%s %s" % [row[3].to_s, row[2].to_s],
-      party: party,
-      party_id: party_id,
-      party__ar: row[5],
+      name:         '%s %s' % [row[1].to_s.tidy, row[0].to_s.tidy],
+      sort_name:    '%s %s' % [row[0].to_s.tidy, row[1].to_s.tidy],
+      given_name:   row[1].to_s.tidy,
+      family_name:  row[0].to_s.tidy,
+      name__ar:     '%s %s' % [row[3].to_s, row[2].to_s],
+      party:        party,
+      party_id:     party_id,
+      party__ar:    row[5],
       constituency: row[6],
-      province: row[8],
-      start_date: date_from(row[14]),
-      end_date: date_from(row[15]),
+      province:     row[8],
+      start_date:   date_from(row[14]),
+      end_date:     date_from(row[15]),
     }
 
     warn "*** #{row[6]} => #{row[7]} not #{arabic[row[6]]} " if arabic[row[6]] && arabic[row[6]] != row[7]
@@ -55,19 +56,19 @@ def reprocess_csv(file)
     arabic[row[8]] = row[9]
 
     if data[:constituency] == 'Jeunes'
-      data[:constituency] = "National"
-      data[:legislative_membership_type] == "Youth Representative"
+      data[:constituency] = 'National'
+      data[:legislative_membership_type] == 'Youth Representative'
     end
 
     if data[:constituency] == 'Femmes'
-      data[:constituency] = "National"
+      data[:constituency] = 'National'
       data[:legislative_membership_type] == "Women's Representative"
-      data[:gender] = "female"
+      data[:gender] = 'female'
     end
 
-    # TODO: we need a better ID surrogate, 
+    # TODO: we need a better ID surrogate,
     # as there are multiple people with the same name
-    ScraperWiki.save_sqlite([:name, :constituency], data)
+    ScraperWiki.save_sqlite(%i(name constituency), data)
   end
 end
 
